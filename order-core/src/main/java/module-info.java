11e5e9dd668203c392e8b318627f0ec6461289d8
Aery <nulllifeart@gmail.com>
2024-06-09 12:15:37 +0800

    fix "aery.practice.order.core cannot" access unnamed module "spring.core", just need requires it.

--- order-core/src/main/java/module-info.java
+++ order-core/src/main/java/module-info.java
@@ -4,6 +4,7 @@ open module aery.practice.order.core {
 
     requires transitive aery.practice.order.api;
     requires aery.practice.order.utils;
+//    requires spring.core; // 自動模組, 不引入的話會無法執行, 因為 AOP 會反向從這個模組要操作到這個 module, 拿掉的話會看到無法存取無名模組的訊息
     requires spring.context;
     requires spring.boot;
     requires spring.boot.autoconfigure;
--- order-portal/src/main/java/org/aery/practice/order/portal/OrderMatcherIntegrationTesting.java
+++ order-portal/src/main/java/org/aery/practice/order/portal/OrderMatcherIntegrationTesting.java
@@ -2,15 +2,10 @@ package org.aery.practice.order.portal;
 
 import org.aery.practice.order.api.OrderMatcher;
 
-/**
- * --add-reads aery.practice.order.core=ALL-UNNAMED
- * --module-path mods:libs
- */
 public class OrderMatcherIntegrationTesting {
 
     public static void main(String[] args) {
-        Module u = ClassLoader.getPlatformClassLoader().getUnnamedModule();
-        OrderMatcher orderMatcher1 = org.aery.practice.order.api.OrderMatcher.load();
+        OrderMatcher orderMatcher1 = OrderMatcher.load();
         orderMatcher1.start(args);
     }
 
